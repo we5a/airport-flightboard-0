a8c03ae8864fcfa4b7717e000b0234e4493e59de
Yaroslav_S <leafset@gmail.com>
2021-04-16 19:04:43 +0300

    add routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require('./db');
+const flightRouter = require('./routes/flight-board-router');
 const app = express();
 const apiPort = 3000;
 
@@ -14,5 +15,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
+
+app.use('/api', flightRouter);
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
---
+++ server/routes/flight-board-router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const FlightCtrl = require('../controllers/flight-ctrl.js');
+
+const router = express.Router();
+
+router.post('/flight', FlightCtrl.addFlight);
+router.put('/flight/:id', FlightCtrl.updateFlight);
+router.delete('/flight/:id', FlightCtrl.deleteFlight);
+router.get('/flights', FlightCtrl.getFlights);
+
+module.exports = router;
